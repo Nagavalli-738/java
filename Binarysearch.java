272d9c48d32987ff0a6590bb4dc51f62e864d9a4
Nagavalli-738 <151906536+Nagavalli-738@users.noreply.github.com>
2024-11-08 09:46:37 +0530

    Binarysearch.java

--- Binarysearch.java
+++ Binarysearch.java
@@ -1,43 +1,44 @@
+import java.util.*;
 import java.io.*;
-import java.util.Scanner;
-public class Binarysearch{
-    public static void main(String args[]){
-        Scanner sc=new Scanner(System.in);
-        int pos=-1;
-        System.out.println("Enter size of an array:");
-        int n=sc.nextInt();
-        int arr[]=new int[n];
-        System.out.println("Enter"+n+"values of array is sorted array");
-        for(int i=0;i<n;i++)
-        {
-            arr[i]=sc.nextInt();
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int pos = -1;
+        System.out.println("Enter number of elements of array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter " + n + " values: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
         }
-        System.out.println("Enter key element to search: ");
-        int key=sc.nextInt();
-        int low=0,high=n-1;
-        while(low<high){
-            int mid=(low+high)/2;
-            if(arr[mid]==key)
-            {
-                pos=mid+1;
+        System.out.println("Enter the key element to search: ");
+        int key = sc.nextInt();
+        int low = 0;
+        int high = n - 1;
+        Arrays.sort(arr);
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (arr[mid] == key) {
+                pos = mid + 1;
                 break;
+            } else if (arr[mid] > key) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
-            else if(arr[mid]<key)
-            {
-                low=mid+1;
-            }
-            else
-            {
-                high=mid-1;
-            }
-            }
-            if(pos==-1)
-            {
-                System.out.println(key+"not found search falied");
-            }
-            else
-            {
-                System.out.println(key+"found at"+pos+"postion in the array");
-            }
+        }
+        if (pos == -1) {
+            System.out.println("Search unsuccessful");
+        } else {
+            System.out.println("Element found at position " + pos + " in the array.");
+        }
     }
 }
+output:
+Enter number of elements of array: 
+5
+Enter 5 values: 
+10 20 30 40 50
+Enter the key element to search: 
+20
+Element found at position 2 in the array.
